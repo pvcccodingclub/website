76c6dc94b7be43175b4293e74ab57f47fe085272
Ryan <ryanmichaelgarber@gmail.com>
2025-11-30 20:14:55 -0500

    Revert to static output

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
     site: "https://www.pvcccodingclub.workers.dev",
-    output: "server",
+    output: "static",
     integrations: [mdx(), sitemap()],
     adapter: cloudflare({
         platformProxy: {
